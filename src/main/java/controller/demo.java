bf231690d96e8971d19747b2be302f08546f9107
sum7352 <sum734772@gmail.com>
2026-03-12 00:41:13 +0530

    sample comit

--- src/main/java/controller/demo.java
+++ src/main/java/controller/demo.java
@@ -2,6 +2,6 @@ package controller;
 
 public class demo {
  public static void main(String[] args){
-     System.out.println("sumit hello  pandey commit");
+     System.out.println("sumit hello VCXHCUVVHUVFVF pandey commit");
  }
 }
